1835a983e9ae5ea71ff364f96c155a536575af4e
peanutChowder <peanutchowder@gmail.com>
2022-11-20 02:16:09 -0700

    Removed clutter, i.e. inline html scripts, unused content scripts

--- newTab.js
+++ newTab.js
@@ -1,19 +1,42 @@
 const taskList = document.getElementById("task-list")
 const taskEntry = document.getElementById("task-entry")
+var tasks = []
 
-document.getElementById("task-form").addEventListener("submit", (e) => addTask(e))
+chrome.storage.sync.get(['tasks'], (result) => {
+    if (result.tasks != null) {
+        tasks = result.tasks
+        tasks.forEach(task => {
+            const taskItem = createNewTask(task)
+            addTaskToDOM(taskItem)
+        });
+    }
+})
 
+document.getElementById("task-form").addEventListener("submit", (e) => addNewTask(e))
 
+function addTaskToDOM(taskItem) {
+    taskList.appendChild(taskItem)
+}
 
-function addTask(e) {
+function addNewTask(e) {
     e.preventDefault()
-    console.log("Enter")
-    taskList.appendChild(createNewTask(taskEntry.value))
+    
+    if (taskEntry.value == "") {
+        return
+    }
+
+    const newTask = createNewTask({id: tasks.length, text: taskEntry.value})
+    addTaskToDOM(newTask)
+
+
+    tasks.push({id: tasks.length - 1, text: taskEntry.value})
+    chrome.storage.sync.set({"tasks": tasks})
+
     taskEntry.value = ""
     return false
 }
 
-function createNewTask(taskText) {
+function createNewTask(taskObj) {
     let newTask = document.createElement("li")
     let checkbox = document.createElement("input")
 
@@ -21,12 +44,15 @@ function createNewTask(taskText) {
 
     checkbox.type = "checkbox"
     checkbox.className = "task-item-checkbox"
+
     checkbox.addEventListener("click", () => {
+        tasks = tasks.filter((task) => task.id != taskObj.id)
+        chrome.storage.sync.set({"tasks": tasks})
         newTask.remove()
     })
 
     newTask.appendChild(checkbox)
-    newTask.appendChild(document.createTextNode(taskText))
+    newTask.appendChild(document.createTextNode(taskObj.text))
 
     return newTask
 }
